a0c751d2fa00958d404872b67d6429f8b7881f35
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-25 15:48:45 +0100

    Create cron.php

---
+++ cron.php
@@ -0,0 +1,12 @@
+<?php
+echo "cron<br/>";
+/*
+lubw_collector.php
+collector.php
+luftdaten_crawler.php
+generate_districts_past.php
+markersMapped.php
+chronological_data_of_districts.php
+*/
+
+?>
