3b0af2dd40429877d9e57db0472fa076b0791c04
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-26 12:44:12 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,4 +1,5 @@
 <?php
+include_once("library.php");
 echo "cron<br/>";
 /*
 lubw_collector.php
@@ -8,5 +9,9 @@ generate_districts_past.php
 markersMapped.php
 chronological_data_of_districts.php
 */
+
+$sql = "SELECT * FROM `cron_jobs` WHERE NOW() > DATE_ADD(`last_execution`, INTERVAL `interval` SECOND)";
+debug_query($sql);
+
 
 ?>
