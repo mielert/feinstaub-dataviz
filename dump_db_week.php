132f24c94935b5ef275c3a495189caa09c37f014
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-26 17:59:04 +0100

    Update dump_db_week.php

--- dump_db_week.php
+++ dump_db_week.php
@@ -1,4 +1,13 @@
 <?php
+include("library.php");
+$simple_file = $data_root."city_week.tsv";
 
+  $sql = "SELECT *
+FROM `sensors_hourly_mean`  
+LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`lon` = `sensors_hourly_mean`.`lon` AND `x_coordinates_districts`.`lat` = `sensors_hourly_mean`.`lat`
+WHERE `sensors_hourly_mean`.`timestamp` = "2017-01-26 16:00:00"
+AND `x_coordinates_districts`.`district_id` > 0  
+ORDER BY `sensors_hourly_mean`.`sensor_id` ASC";
+  
 
 ?>
