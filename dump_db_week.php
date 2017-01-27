02f2b9c3b12c21e523913cef6fbd6ed68c080392
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-27 20:14:35 +0100

    Update dump_db_week.php

--- dump_db_week.php
+++ dump_db_week.php
@@ -3,7 +3,9 @@
 include("library.php");
 $simple_file = $data_root."city_week.tsv";
 
-$sql = "SELECT MAX(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = 1";
+$city_id = 1;
+
+$sql = "SELECT MAX(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
 $result = debug_query($sql);
 if(count($result) > 0 && $result[0]->timestamp > "1000-01-01 00:00:00")
   $start_timestamp = $result[0]->timestamp;
@@ -14,7 +16,7 @@ else{
             ON `x_coordinates_districts`.`lon` = `sensors_hourly_mean`.`lon` 
             AND `x_coordinates_districts`.`lat` = `sensors_hourly_mean`.`lat`
           LEFT JOIN `districts` ON `districts`.`id` = `x_coordinates_districts`.`district_id`
-          WHERE `districts`.`city_id` = 1";
+          WHERE `districts`.`city_id` = $city_id";
   $result = debug_query($sql);
   $start_timestamp = $result[0]->timestamp;
 }
@@ -24,33 +26,89 @@ $sql = "SELECT DISTINCT `timestamp`
           ON `x_coordinates_districts`.`lon` = `sensors_hourly_mean`.`lon` 
           AND `x_coordinates_districts`.`lat` = `sensors_hourly_mean`.`lat`
         LEFT JOIN `districts` ON `districts`.`id` = `x_coordinates_districts`.`district_id`
-        WHERE `districts`.`city_id` = 1
+        WHERE `districts`.`city_id` = $city_id
         ORDER BY `timestamp`";
 $results = debug_query($sql);
 $stop_timestamp = $result[count($result)-1]->timestamp;
 
+$counter = 0;
 foreach($results as $result){
   $timestamp = $result->timestamp;
   $sql = "SELECT *
-FROM `sensors_hourly_mean`  
-LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`lon` = `sensors_hourly_mean`.`lon` AND `x_coordinates_districts`.`lat` = `sensors_hourly_mean`.`lat`
-WHERE `sensors_hourly_mean`.`timestamp` = '$timestamp'
-AND `x_coordinates_districts`.`district_id` > 0  
-ORDER BY `sensors_hourly_mean`.`sensor_id` ASC";
-  
+          FROM `sensors_hourly_mean`  
+          LEFT JOIN `x_coordinates_districts` 
+            ON `x_coordinates_districts`.`lon` = `sensors_hourly_mean`.`lon` 
+            AND `x_coordinates_districts`.`lat` = `sensors_hourly_mean`.`lat`
+          WHERE `sensors_hourly_mean`.`timestamp` = '$timestamp'
+          LEFT JOIN `districts` ON `districts`.`id` = `x_coordinates_districts`.`district_id`
+          WHERE `districts`.`city_id` = $city_id
+          ORDER BY `sensors_hourly_mean`.`sensor_id` ASC";
   $result = debug_query($sql);
-  $data = get_min_max_mid($result,$timestamp);
+  $data = get_min_max_mid($result);
   print_r($data);
-  break;
+  save_min_max_mid($data,$timestamp,count($result),$city_id);
+  if($counter>10) break;
+  $counter++;
 }
 /**
  *
  */
+function save_min_max_mid($data,$timestamp,$num_sensors,$city_id){
+  $sql = "INSERT INTO `cities_mean` 
+          ( `id`,
+            `city_id`,
+            `timestamp`,
+            `P1h`,
+            `P2h`,
+            `P1d`,
+            `P2d`,
+            `P1h_min`,
+            `P1h_max`,
+            `P2h_min`,
+            `P2h_max`,
+            `P1h_50_min`,
+            `P1h_50_max`,
+            `P2h_50_min`,
+            `P2h_50_max`,
+            `P1min_sensor_id`,
+            `P1max_sensor_id`,
+            `P2min_sensor_id`,
+            `P2max_sensor_id`)
+				  VALUES 
+          ( NULL,
+            $city_id,
+            '$timestamp',
+            ".$data["P1"]["mid"].",
+            ".$data["P2"]["mid"].",
+            ".$data["P1"]["d_mid"].",
+            ".$data["P2"]["d_mid"].",
+            ".$data["P1"]["min"].",
+            ".$data["P1"]["max"].",
+            ".$data["P2"]["min"].",
+            ".$data["P2"]["max"].",
+            ".$data["P1"]["min_main"].",
+            ".$data["P1"]["max_main"].",
+            ".$data["P2"]["min_main"].",
+            ".$data["P2"]["max_main"].",
+            ".$data["P1"]["min_sensor_id"].",
+            ".$data["P1"]["max_sensor_id"].",
+            ".$data["P2"]["min_sensor_id"].",
+            ".$data["P2"]["max_sensor_id"]."
+          )
+				  ON DUPLICATE KEY UPDATE 
+            `P1` = VALUES(`P1`), 
+            `P2` = VALUES(`P2`); ";
+  
+  ";
+}
+/**
+ * $data["P1"]["d_mid"].",
+ *           ".$data["P2"]["d_mid"].",
+ * fehlen
+ */
 function get_min_max_mid($data,$timestamp){
     //echo "get_min_max_mid";
-    $statistics = array("timestamp"=>$timestamp,
-                        "num_sensors"=>0,
-                        "P1"=>array("min"=>1000000,
+    $statistics = array("P1"=>array("min"=>1000000,
                                     "max"=>0,
                                     "mid"=>0,
                                     "max_main"=>"",
