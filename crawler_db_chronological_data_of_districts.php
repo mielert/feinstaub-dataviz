7dc03816f2044492a8885606415869c87eedb324
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-28 11:08:07 +0100

    Update crawler_db_chronological_data_of_districts.php

--- crawler_db_chronological_data_of_districts.php
+++ crawler_db_chronological_data_of_districts.php
@@ -1,7 +1,7 @@
 
 <html>
 	<head>
-		<script src="../js/jquery.min.js" type="text/javascript"></script>
+		<script src="js/jquery.min.js" type="text/javascript"></script>
 	</head>
 	<body>
 		<pre>
