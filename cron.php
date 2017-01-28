8e15b9aece6556147dee0c6604bf4a04ba6d56ab
Fritz Mielert <fritz.mielert@gmx.de>
2017-01-28 11:20:57 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -11,11 +11,11 @@ chronological_data_of_districts.php
 */
 
 $sql = "SELECT * FROM `cron_jobs` WHERE NOW() > DATE_ADD(`last_execution`, INTERVAL `interval` SECOND) OR `last_execution` IS NULL LIMIT 1";
-$result = debug_query($sql);
-if(count($result)>0){
-	$result = include_once($result[0]->script);
+$to_run = debug_query($sql);
+if(count($to_run)>0){
+	$result = include_once($to_run[0]->script);
 	if($result){
-		$sql = "UPDATE `cron_jobs` SET `last_execution` = NOW() WHERE `id` = ".$result[0]->id;
+		$sql = "UPDATE `cron_jobs` SET `last_execution` = NOW() WHERE `id` = ".$to_run[0]->id;
 		$result = debug_query($sql);
 	}
 	else echo "error";
