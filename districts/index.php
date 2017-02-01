453687e4b741d6355742cab0b50e8f721b20fac0
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-01 16:35:38 +0100

    Update index.php

--- districts/index.php
+++ districts/index.php
@@ -549,7 +549,9 @@ var view = graph.append("rect")
 			data2map(mousemove(d3.mouse(this)));
 			}
 		);
-data2map([0,100]);  
+//data2map([0,100]);
+//alert(d3.max(data, function(d) { return d.timestamp; }));
+$("#mapTimeInfo").html("P10: 24h-Mittel am "+hoverTimeFormat(d3.max(data, function(d) { return d.timestamp; })));
 
 /**
  *
