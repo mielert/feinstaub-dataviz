960c802773d770ae51882d4422bd96692fedf440
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-07 13:30:49 +0100

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -2,6 +2,6 @@
 include_once("library.php");
 
 $city_id = 1;
-$sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city__id` = $city_id)";
+$sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
 debug_query($sql);
 ?>
