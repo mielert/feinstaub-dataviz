035535fb318c7614f4548e1eabbb4d15a310d507
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-07 13:45:23 +0100

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -5,8 +5,17 @@ $city_id = 1;
 
 $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
 $result = debug_query($sql);
-if($result == array()) echo "empty";
-
+if($result[0]->timestamp == "") {
+  echo "cities_mean for $city_id is empty";
+  $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
+  $result = debug_query($sql);
+  $starttime = $result[0]->timestamp;
+}
+else{
+  $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id AND `timestamp` > '".$result[0]->timestamp."')";
+  $result = debug_query($sql);
+  $starttime = $result[0]->timestamp;
+}
 $sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
-debug_query($sql);
+//debug_query($sql);
 ?>
