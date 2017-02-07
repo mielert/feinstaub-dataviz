51f5cb8111c4654097e0b1918f0610522930a30b
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-07 13:48:39 +0100

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -3,10 +3,11 @@ include_once("library.php");
 
 $city_id = 1;
 
+$starttime = false;
 $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
 $result = debug_query($sql);
 if($result[0]->timestamp == "") {
-  echo "cities_mean for $city_id is empty";
+  echo "cities_mean for city_id = $city_id is empty";
   $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
   $result = debug_query($sql);
   $starttime = $result[0]->timestamp;
@@ -16,6 +17,11 @@ else{
   $result = debug_query($sql);
   $starttime = $result[0]->timestamp;
 }
-$sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
-//debug_query($sql);
+if(!$starttime){
+  echo "no start time because of empty table districts_mean at city_id = $city_id";
+  exit;
+}
+// get district data
+$sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id) AND `timestamp` = '$starttime'";
+debug_query($sql);
 ?>
