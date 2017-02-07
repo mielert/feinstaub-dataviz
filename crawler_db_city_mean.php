b5eb73c872222fec6222b5741b0b79027c382574
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-07 13:38:05 +0100

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -2,6 +2,11 @@
 include_once("library.php");
 
 $city_id = 1;
+
+$sql = "SELECT MIN(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
+$result = debug_query($sql);
+if($result == array()) echo "empty";
+
 $sql = "SELECT * FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id)";
 debug_query($sql);
 ?>
