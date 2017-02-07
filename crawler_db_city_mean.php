5e5f5f0d00bd553176addee3e5035fe1020243d7
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-07 15:23:32 +0100

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -6,7 +6,7 @@ $city_id = 1;
 // get start time
 $starttime = false;
 $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
-$result = db_select($sql);
+$result = debug_query($sql);
 if($result[0]->timestamp == "") {
   echo "cities_mean for city_id = $city_id is empty";
   $sql = "SELECT MIN(`timestamp`) AS timestamp 
@@ -19,12 +19,12 @@ if($result[0]->timestamp == "") {
                           FROM `districts`
                           LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
                           WHERE `districts`.`city_id` = $city_id)";
-  $result = db_select($sql);
+  $result = debug_query($sql);
   $starttime = $result[0]->timestamp;
 }
 else{
   $sql = "SELECT MIN(`timestamp`) AS timestamp FROM `districts_mean` WHERE `district_id` IN (SELECT `id` FROM `districts` WHERE `city_id` = $city_id AND `timestamp` > '".$result[0]->timestamp."')";
-  $result = db_select($sql);
+  $result = debug_query($sql);
   $starttime = $result[0]->timestamp;
 }
 if(!$starttime){
@@ -45,11 +45,11 @@ $sql = "SELECT *
                           WHERE `districts`.`city_id` = $city_id)
           AND `sensor_data`.`timestamp` >  '".substr($starttime,0,13).":00:00'
           AND `sensor_data`.`timestamp` <= DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR)";
-$result = db_select($sql);
+$result = debug_query($sql);
 
 // calculate city data
 $statistics = get_min_max_mid($result);
-$result = db_select("SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR) AS timestamp");
+$result = debug_query("SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR) AS timestamp");
 $statistics["timestamp"] = $result[0]->timestamp;
 echo "<pre>".print_r($statistics,true)."</pre>";
 $sql = "INSERT INTO `cities_mean` (`id`, `city_id`, `timestamp`, `P1h`, `P2h`, `P1d`, `P2d`, `P1h_min`, `P1h_max`, `P2h_min`, `P2h_max`, `P1h_50_min`, `P1h_50_max`, `P2h_50_min`, `P2h_50_max`, `P1min_sensor_id`, `P1max_sensor_id`, `P2min_sensor_id`, `P2max_sensor_id`, `num_sensors`, `num_values`)
