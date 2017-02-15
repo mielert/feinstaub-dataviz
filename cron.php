8a8fc5f636742a4a96ad57733c27351ff7806d03
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-15 17:29:09 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,4 +1,4 @@
-<?php if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR“]) exit; ?>
+<?php if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; ?>
 <?php
 include_once("library.php");
 echo "cron<br/>";
