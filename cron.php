297d86b8bc6ed2b58e224de55ef8e7df2d7286e5
Fritz Mielert <fritz.mielert@gmx.de>
2017-02-17 11:54:39 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,30 +1,21 @@
 <?php if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; ?>
 <?php
 include_once("library.php");
-echo "cron<br/>";
-/*
-lubw_collector.php
-collector.php
-luftdaten_crawler.php
-generate_districts_past.php
-markersMapped.php
-chronological_data_of_districts.php
-*/
 
-$sql = "SELECT * 
+$sql = "SELECT * ,(NOW()-DATE_ADD(`last_execution`, INTERVAL `interval` SECOND))
 	FROM `cron_jobs` 
 	WHERE (NOW() > DATE_ADD(`last_execution`, INTERVAL `interval` SECOND) 
 	OR `last_execution` IS NULL)
 	AND activated = 1
-	LIMIT 1";
+    ORDER BY (NOW()-DATE_ADD(`last_execution`, INTERVAL `interval` SECOND)) DESC";
 $to_run = debug_query($sql);
 if(count($to_run)>0){
+	$sql = "UPDATE `cron_jobs` SET `last_execution` = NOW() WHERE `id` = ".$to_run[0]->id;
+	$result = debug_query($sql);
 	$result = include_once($to_run[0]->script);
-	if($result){
-		$sql = "UPDATE `cron_jobs` SET `last_execution` = NOW() WHERE `id` = ".$to_run[0]->id;
-		$result = debug_query($sql);
-	}
-	else echo "error";
+	if($result) $sql = "UPDATE `cron_jobs` SET `last_result` = 1 WHERE `id` = ".$to_run[0]->id;
+	else $sql = "UPDATE `cron_jobs` SET `last_result` = -1 WHERE `id` = ".$to_run[0]->id;
+	$result = debug_query($sql);
 }
 
 
