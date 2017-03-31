09b07faa8bf1f1ce22da1e25cfa8d84a3221696c
Fritz Mielert <fritz.mielert@gmx.de>
2017-03-31 11:16:00 +0200

    Update crawler_luftdaten_api.php

--- crawler_luftdaten_api.php
+++ crawler_luftdaten_api.php
@@ -1,10 +1,12 @@
-<pre><?php if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; ?>
 <?php
+if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; 
 include_once("library.php");
+$log = false;
 /**
  *
  */
 function read_data_via_api(){
+	global $script_result;
     // Setup cURL
     /*
      9.1.2017 API-Changes
@@ -27,7 +29,8 @@ Vary: Accept
 https://api.luftdaten.info/v1/now/
 https://api.madavi.de/v1/now/?format=json
     */
-    $url = "https://api.luftdaten.info/v1/now/?format=json";
+	
+    $url = "http://api.luftdaten.info/v1/now/?format=json";
     $ch = curl_init($url);
     curl_setopt_array($ch, array(
         CURLOPT_POST => FALSE,
@@ -44,17 +47,20 @@ https://api.madavi.de/v1/now/?format=json
     // Check for errors
     if($response === FALSE){
         mail("fritz.mielert@gmx.de","Collector Error","Curl Error: ".curl_error($ch));
-        die(curl_error($ch));
+        //die(curl_error($ch));
+		$script_result = "Curl Error: ".curl_error($ch);
         return false;
     }
     else{
         if(strpos($response,"nginx")!==false){
             $response = substr($response,strpos($response,"<title>")+7);
             $response = substr($response,0,strpos($response,"</title>"));
-            echo "<h1>Error: $response</h1>";
-            echo "Couldn't fetch $url";
+            //echo "<h1>Error: $response</h1>";
+            //echo "Couldn't fetch $url";
             mail("fritz.mielert@gmx.de","Collector Error: $response",$response);
-            exit;
+			$script_result = "Error: $response Couldn't fetch $url";
+            //exit;
+			return $false;
         }
         else{
             // Decode the response
@@ -62,6 +68,17 @@ https://api.madavi.de/v1/now/?format=json
             return $responseData;
         }
     }
+    
+	/*
+	global $data_root;
+	$data = file_get_contents($data_root."apidata.json");
+	//echo strlen($data);
+	if(strlen($data) > 0){
+		$responseData = json_decode($data, TRUE);
+		//print_r($responseData,true);
+		return $responseData;
+	}
+	*/
 }
 /**
  *
@@ -79,10 +96,15 @@ function remove_sensors_not_sds($data){
  *
  */
 function prepare_data_for_db($data){
-	date_default_timezone_set('Europe/Gibraltar');
+	global $log;
+	global $script_result;
 	if(count($data)>0){
 		$sql = array();
+		$counter = 0;
+		$script_result.= " (first: ".date("Y-m-d H:i:s",strtotime(str_replace("+00:00","+01:00",$data[count($data)-1]["timestamp"]))).", ";
+		$script_result.= "last: ".date("Y-m-d H:i:s",strtotime(str_replace("+00:00","+01:00",$data[0]["timestamp"]))).") ";
 		foreach($data as $dataset){
+			if($log) file_put_contents("crawler_luftdaten_api.log", date('Y-m-d H:i:s')."	r103, processing $counter\n", FILE_APPEND | LOCK_EX);
             $P1 = 0;
             $P2 = 0;
             foreach($dataset["sensordatavalues"] as $values){
@@ -103,6 +125,7 @@ function prepare_data_for_db($data){
 				data_to_db($sql);
 				$sql = array();
 			}
+			$counter++;
 		}
 		if(count($sql)>0){
 			data_to_db($sql);
@@ -113,19 +136,27 @@ function prepare_data_for_db($data){
  *
  */
 function data_to_db($sql){
-	echo count($sql);
+	//echo count($sql);
 	$sql = join(",\n",$sql);
 	$sql = "INSERT INTO `sensor_data`
 			(`id`, `sensor_id`, `lon`, `lat`, `timestamp`, `P1`, `P2`)
 			VALUES ".$sql."
 			ON DUPLICATE KEY UPDATE
 			`P1` = Values(`P1`), `P2` = Values(`P2`)";
-	return debug_query($sql);
+	return db_insert($sql);
 }
 // read data via api
+if($log) file_put_contents("crawler_luftdaten_api.log", date('Y-m-d H:i:s')."	r142, start\n", FILE_APPEND | LOCK_EX);
+
 $data = read_data_via_api();
-// sds011 cleanup
-$data = remove_sensors_not_sds($data);
-// save to db
-prepare_data_for_db($data);
-?></pre>
+if($data){
+	if($log) file_put_contents("crawler_luftdaten_api.log", date('Y-m-d H:i:s')."	r146, got data -> ".count($data)."\n", FILE_APPEND | LOCK_EX);
+	// sds011 cleanup
+	$data = remove_sensors_not_sds($data);
+	if($log) file_put_contents("crawler_luftdaten_api.log", date('Y-m-d H:i:s')."	r149, only sds -> ".count($data)."\n", FILE_APPEND | LOCK_EX);
+	$script_result = count($data)." datasets";
+	// save to db
+	prepare_data_for_db($data);
+}
+echo $script_result;
+?>
