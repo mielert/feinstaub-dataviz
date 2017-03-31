1baa8be6198e8876014fe0d17e462be0062923a8
Fritz Mielert <fritz.mielert@gmx.de>
2017-03-31 11:12:51 +0200

    Update crawler_db_city_mean.php

--- crawler_db_city_mean.php
+++ crawler_db_city_mean.php
@@ -1,99 +1,144 @@
-<?php if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; ?>
 <?php
-include_once("library.php");
+if($_SERVER["REMOTE_ADDR"] !== $_SERVER["SERVER_ADDR"]) exit; 
 
+include_once("library.php");
+$log = false;
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r6, start\n", FILE_APPEND | LOCK_EX);
+
+$parent_region_id = 24;
 $city_id = 1;
 
 // get start time
 $starttime = false;
 $sql = "SELECT MAX(`timestamp`) AS timestamp FROM `cities_mean` WHERE `city_id` = $city_id";
-$result = debug_query($sql);
+$result = db_select($sql);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r15, $sql\n", FILE_APPEND | LOCK_EX);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r16, ".$result[0]->timestamp."\n", FILE_APPEND | LOCK_EX);
 if($result[0]->timestamp == "") {
-  echo "cities_mean for city_id = $city_id is empty";
+  //echo "cities_mean for city_id = $city_id is empty";
   $sql = "SELECT MIN(`timestamp`) AS timestamp 
           FROM `sensor_data` 
           LEFT JOIN `sensors` ON `sensors`.`id` = `sensor_data`.`sensor_id`
           WHERE `lon` IN (SELECT `lon` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `lat` IN (SELECT `lat` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `sensors`.`type_id` = 1";
-  $result = debug_query($sql);
+  $result = db_select($sql);
   $starttime = $result[0]->timestamp;
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r15, $sql\n", FILE_APPEND | LOCK_EX);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r16, ".$result[0]->timestamp."\n", FILE_APPEND | LOCK_EX);
 }
 else{
   $sql = "SELECT MIN(`timestamp`) AS timestamp 
           FROM `sensor_data` 
           LEFT JOIN `sensors` ON `sensors`.`id` = `sensor_data`.`sensor_id`
           WHERE `lon` IN (SELECT `lon` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `lat` IN (SELECT `lat` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `sensors`.`type_id` = 1
           AND `timestamp` > '".$result[0]->timestamp."'";
-  $result = debug_query($sql);
+  $result = db_select($sql);
   $starttime = $result[0]->timestamp;
+  if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r15, $sql\n", FILE_APPEND | LOCK_EX);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r16, ".$result[0]->timestamp."\n", FILE_APPEND | LOCK_EX);
+
 }
+//$starttime = "2017-03-21 12:00:00";
 if(!$starttime){
-  echo "no start time because of empty table sensor_data at city_id = $city_id";
+  echo "nothing to do with parent_region_id = $parent_region_id (timestamp: $starttime)";
+  if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r52, nothing to do with parent_region_id = $parent_region_id (timestamp: $starttime)\n", FILE_APPEND | LOCK_EX);
   exit;
 }
 else {
-  echo "start time of city_id = $city_id = $starttime";
+  echo "start time of city_id = $city_id = $starttime ";
+  if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r57, start time of city_id = $city_id = $starttime\n", FILE_APPEND | LOCK_EX);
   $sql = "SELECT DATE_ADD(MAX(timestamp), INTERVAL -1 HOUR) AS timestamp 
           FROM `sensor_data`
           LEFT JOIN `sensors` ON `sensors`.`id` = `sensor_data`.`sensor_id`
           WHERE `sensors`.`type_id` = 1";
-  $result = debug_query($sql);
+  $result = db_select($sql);
   $endtime = $result[0]->timestamp;
   if($starttime > $endtime) {
-    echo "$starttime > $endtime ";
+    echo "$starttime > $endtime (DATE_ADD(MAX(timestamp), INTERVAL -1 HOUR) FROM `sensor_data`) ";
     echo "nothing to do ";
+    if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r67, $starttime > $endtime (DATE_ADD(MAX(timestamp), INTERVAL -1 HOUR) FROM `sensor_data`) nothing to do \n", FILE_APPEND | LOCK_EX);
     exit;
   }
 }
+/*
+$sql = "SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR) AS added";
+$result = db_select($sql);
+echo $result[0]->added;
+$sql = "SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 2 HOUR) AS added";
+$result = db_select($sql);
+echo $result[0]->added;
+*/
+//if($result[0]->added == substr($starttime,0,13).":00:00")
 // get sensor data
 $sql = "SELECT * 
           FROM `sensor_data` 
           LEFT JOIN `sensors` ON `sensors`.`id` = `sensor_data`.`sensor_id`
           WHERE `lon` IN (SELECT `lon` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `lat` IN (SELECT `lat` 
-                          FROM `districts`
-                          LEFT JOIN `x_coordinates_districts` ON `x_coordinates_districts`.`district_id` = `districts`.`id`
-                          WHERE `districts`.`city_id` = $city_id)
+                          FROM `regions`
+                          LEFT JOIN `x_coordinates_regions` ON `x_coordinates_regions`.`region_id` = `regions`.`id`
+                          WHERE `regions`.`parent_region_id` = $parent_region_id)
           AND `sensor_data`.`timestamp` >  '".substr($starttime,0,13).":00:00'
           AND `sensor_data`.`timestamp` <= DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR)
           AND `sensors`.`type_id` = 1";
-$result = debug_query($sql);
+$result = db_select($sql);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r96, $sql\n", FILE_APPEND | LOCK_EX);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r97, ".count($result)."\n", FILE_APPEND | LOCK_EX);
+
+//print_r($result);
 
 // calculate city data
 $statistics = get_min_max_mid($result);
-$result = debug_query("SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR) AS timestamp");
+$result = db_select("SELECT DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR) AS timestamp");
 $statistics["timestamp"] = $result[0]->timestamp;
-echo "<pre>".print_r($statistics,true)."</pre>";
+//echo "<pre>".print_r($statistics,true)."</pre>";
 $sql = "INSERT INTO `cities_mean` (`id`, `city_id`, `timestamp`, `P1h`, `P2h`, `P1d`, `P2d`, `P1h_min`, `P1h_max`, `P2h_min`, `P2h_max`, `P1h_50_min`, `P1h_50_max`, `P2h_50_min`, `P2h_50_max`, `P1min_sensor_id`, `P1max_sensor_id`, `P2min_sensor_id`, `P2max_sensor_id`, `num_sensors`, `num_values`)
         VALUES (NULL, '$city_id', '".$statistics["timestamp"]."', '".$statistics["P1"]["mid"]."', '".$statistics["P2"]["mid"]."', 0, 0, '".$statistics["P1"]["min"]."', '".$statistics["P1"]["max"]."', '".$statistics["P2"]["min"]."', '".$statistics["P2"]["max"]."', '".$statistics["P1"]["max_main"]."', '".$statistics["P1"]["min_main"]."', '".$statistics["P2"]["max_main"]."', '".$statistics["P2"]["min_main"]."', '".$statistics["P1"]["min_sensor_id"]."', '".$statistics["P1"]["max_sensor_id"]."', '".$statistics["P2"]["min_sensor_id"]."', '".$statistics["P2"]["max_sensor_id"]."', ".$statistics["num_sensors"].", ".$statistics["num_values"].")";
-debug_query($sql);
+$res_insert = db_insert($sql);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r116, $sql \n -> $res_insert\n", FILE_APPEND | LOCK_EX);
+// daylight saving time
+if(!$res_insert){
+  $sql = "INSERT INTO `cities_mean` (`id`, `city_id`, `timestamp`, `P1h`, `P2h`, `P1d`, `P2d`, `P1h_min`, `P1h_max`, `P2h_min`, `P2h_max`, `P1h_50_min`, `P1h_50_max`, `P2h_50_min`, `P2h_50_max`, `P1min_sensor_id`, `P1max_sensor_id`, `P2min_sensor_id`, `P2max_sensor_id`, `num_sensors`, `num_values`)
+          VALUES (NULL, '$city_id', DATE_ADD('".$statistics["timestamp"]."',INTERVAL 1 HOUR), '".$statistics["P1"]["mid"]."', '".$statistics["P2"]["mid"]."', 0, 0, '".$statistics["P1"]["min"]."', '".$statistics["P1"]["max"]."', '".$statistics["P2"]["min"]."', '".$statistics["P2"]["max"]."', '".$statistics["P1"]["max_main"]."', '".$statistics["P1"]["min_main"]."', '".$statistics["P2"]["max_main"]."', '".$statistics["P2"]["min_main"]."', '".$statistics["P1"]["min_sensor_id"]."', '".$statistics["P1"]["max_sensor_id"]."', '".$statistics["P2"]["min_sensor_id"]."', '".$statistics["P2"]["max_sensor_id"]."', ".$statistics["num_sensors"].", ".$statistics["num_values"].")";
+  db_insert($sql);
+
+}
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r122, $sql \n -> $res\n", FILE_APPEND | LOCK_EX);
+
 // add 24h floating
 $sql = "SELECT AVG(P1h) AS P1d, AVG(P2h) AS P2d
         FROM `cities_mean` 
         WHERE `city_id` = $city_id
         AND `timestamp` >  DATE_ADD(DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL -1 DAY),INTERVAL 1 HOUR)
         AND `timestamp` <= DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR)";
-$result = debug_query($sql);
-$sql = "UPDATE `cities_mean` SET `P1d` = ".$result[0]->P1d.", `P2d` = ".$result[0]->P2d." WHERE `city_id` = $city_id AND `timestamp` = DATE_ADD('".substr($starttime,0,13).":00:00',INTERVAL 1 HOUR)";
-debug_query($sql);
+$result = db_select($sql);
+if($res_insert)
+  $sql = "UPDATE `cities_mean` SET `P1d` = ".$result[0]->P1d.", `P2d` = ".$result[0]->P2d." WHERE `city_id` = $city_id AND `timestamp` = '".$statistics["timestamp"]."'";
+else
+  $sql = "UPDATE `cities_mean` SET `P1d` = ".$result[0]->P1d.", `P2d` = ".$result[0]->P2d." WHERE `city_id` = $city_id AND `timestamp` = DATE_ADD('".$statistics["timestamp"]."',INTERVAL 1 HOUR)";
+
+$res = db_insert($sql);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r120, $sql \n -> $res\n", FILE_APPEND | LOCK_EX);
+if($log) file_put_contents("crawler_db_city_mean.log", date('Y-m-d H:i:s')."	r121, done.\n\n", FILE_APPEND | LOCK_EX);
+
 /**
  *
  */
@@ -124,7 +169,7 @@ function get_min_max_mid($data){
     $timestamps = array();
     foreach($data as $dataset){
       array_push($timestamps,$dataset->timestamp);
-      echo "<pre>".print_r($dataset,true)."</pre>";
+      //echo "<pre>".print_r($dataset,true)."</pre>";
       if($dataset->P1 < $statistics["P1"]["min"]) {
         $statistics["P1"]["min"] = floatval($dataset->P1);
         $statistics["P1"]["min_sensor_id"] = intval($dataset->sensor_id);
